b5875c0279dccab7a7ee8541058090f89b9be199
Matt Lewis <matthew.lewis@btinternet.com>
2016-02-04 18:01:51 +0000

    Migrated out of diozero-core

---
+++ diozero-provider-jdkdio10/src/main/java/com/diozero/internal/provider/jdkdio10/RPiConstants.java
@@ -0,0 +1,17 @@
+package com.diozero.internal.provider.jdkdio10;
+
+public interface RPiConstants {
+	//static final int I2C_CONTROLLER_NUMBER = 1;
+	// Default address bits size. 7 bits
+	//static final int I2C_ADDRESS_SIZE = I2CDeviceConfig.ADDR_SIZE_7;
+	// Default I2C clock speed is 100Khz - really?!
+	//static final int I2C_DEFAULT_CLOCK = 100_000;
+	//static final int I2C_DEFAULT_CLOCK = DeviceConfig.DEFAULT;
+	
+	// https://projects.drogon.net/understanding-spi-on-the-raspberry-pi/
+	// Default SPI clock speed is 3.4Mhz - really?!
+	//static final int SPI_DEFAULT_CLOCK = 2_000_000;
+	//static final int SPI_DEFAULT_CLOCK = 500_000;
+	//static final int SPI_CLOCK_MODE = 1;
+	//static final int SPI_WORD_LENGTH = 8;
+}
