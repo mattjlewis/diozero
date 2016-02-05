2af9c706db10acc914f9d9d04add8ba06ee6f0e8
Matt Lewis <matthew.lewis@btinternet.com>
2016-02-05 12:22:29 +0000

    Split into separate projects, Maven pom.xml changes

--- diozero-provider-pigpio/src/main/java/com/diozero/internal/provider/pigpioj/PigpioJDigitalInputDevice.java
+++ diozero-provider-pigpio/src/main/java/com/diozero/internal/provider/pigpioj/PigpioJDigitalInputDevice.java
@@ -1,5 +1,32 @@
 package com.diozero.internal.provider.pigpioj;
 
+/*
+ * #%L
+ * Device I/O Zero - pigpioj provider
+ * %%
+ * Copyright (C) 2016 diozero
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -108,16 +135,15 @@ public class PigpioJDigitalInputDevice extends AbstractDevice implements GpioDig
 	}
 
 	@Override
-	public void callback(int pin, boolean value, long time) {
-		long nanos = System.nanoTime();
-		logger.info("callback(" + pin + ", " + value + ", " + time + ")");
+	public void callback(int pin, boolean value, long epochTime, long nanoTime) {
+		logger.info("callback(" + pin + ", " + value + ", " + epochTime + ", " + nanoTime + ")");
 		
 		if (pin != pinNumber) {
 			logger.severe("Error, got a callback for the wrong pin (" + pin + "), was expecting " + pinNumber);
 		}
 		
 		if (listener != null) {
-			listener.valueChanged(new DigitalPinEvent(pin, time, nanos, value));
+			listener.valueChanged(new DigitalPinEvent(pin, epochTime, nanoTime, value));
 		}
 	}
 }
