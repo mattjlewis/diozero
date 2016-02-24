2516a9a63ecd094410909fff5cf89667fecb47a8
Matt Lewis <matthew.lewis@btinternet.com>
2016-02-24 22:43:06 +0000

    JavaDoc fixes

--- diozero-core/src/main/java/com/diozero/sampleapps/LdrControlledLed.java
+++ diozero-core/src/main/java/com/diozero/sampleapps/LdrControlledLed.java
@@ -12,15 +12,15 @@ import com.diozero.util.SleepUtil;
  * Control the brightness of an LED by the brightness of an LDR
  * To run:
  * JDK Device I/O 1.0:
- *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-jdkdio10-0.3-SNAPSHOT.jar:dio-1.0.1-dev-linux-armv6hf.jar -Djava.library.path=. com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 18
+ *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-jdkdio10-0.3-SNAPSHOT.jar:dio-1.0.1-dev-linux-armv6hf.jar -Djava.library.path=. com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 12
  * JDK Device I/O 1.1:
- *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-jdkdio11-0.3-SNAPSHOT.jar:dio-1.1-dev-linux-armv6hf.jar -Djava.library.path=. com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 18
+ *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-jdkdio11-0.3-SNAPSHOT.jar:dio-1.1-dev-linux-armv6hf.jar -Djava.library.path=. com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 12
  * Pi4j:
- *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-pi4j-0.3-SNAPSHOT.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 18
+ *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-pi4j-0.3-SNAPSHOT.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 12
  * wiringPi:
- *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-wiringpi-0.3-SNAPSHOT.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 18
+ *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-wiringpi-0.3-SNAPSHOT.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 12
  * pigpgioJ:
- *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-pigpio-0.3-SNAPSHOT.jar:pigpioj-java-1.0.0.jar com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 18
+ *  sudo java -cp tinylog-1.0.3.jar:diozero-core-0.3-SNAPSHOT.jar:diozero-provider-pigpio-0.3-SNAPSHOT.jar:pigpioj-java-1.0.0.jar com.diozero.sampleapps.LdrControlledLed MCP3208 0 3 12
  */
 public class LdrControlledLed {
 	public static void main(String[] args) {
@@ -46,8 +46,8 @@ public class LdrControlledLed {
 	
 	public static void test(McpAdc.Type type, int chipSelect, int pin, float vRef, int r1, int ledPin) {
 		try (McpAdc adc = new McpAdc(type, chipSelect); LDR ldr = new LDR(adc, pin, vRef, r1); PwmLed led = new PwmLed(ledPin)) {
-			// Detect variations of 5%
-			ldr.addListener(.05f, (event) -> led.setValue(1-event.getUnscaledValue()));
+			// Detect variations of 5%, poll every 20ms
+			ldr.addListener((event) -> led.setValue(1-event.getUnscaledValue()), .05f, 20);
 			
 			Logger.debug("Sleeping for 20s");
 			SleepUtil.sleepSeconds(20);
---
+++ diozero-core/src/main/java/com/diozero/sampleapps/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Sample applications.
+ */
+package com.diozero.sampleapps;
