4937ca06c8fd0aaf15e42cbbe7fb5e0fbadd1145
mattjlewis <matthew.lewis@btinternet.com>
2016-03-06 20:05:26 +0000

    Extra common IMU methods

--- diozero-core/src/main/java/com/diozero/api/imu/ImuInterface.java
+++ diozero-core/src/main/java/com/diozero/api/imu/ImuInterface.java
@@ -5,8 +5,23 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import com.diozero.util.RuntimeIOException;
 
 public interface ImuInterface {
-	ImuData getIMUData() throws RuntimeIOException;
+	
+	String getImuName();
+	/** Get the recommended poll interval in mS */
+	int getPollInterval();
+	
+	boolean hasGyro();
+	boolean hasAccelerometer();
+	boolean hasCompass();
+	
+	void startRead();
+	void stopRead();
+	
+	ImuData getImuData() throws RuntimeIOException;
 	Vector3D getGyroData() throws RuntimeIOException;
 	Vector3D getAccelerometerData() throws RuntimeIOException;
 	Vector3D getCompassData() throws RuntimeIOException;
+	
+	void addTapListener(TapListener listener);
+	void addOrientationListener(OrientationListener listener);
 }
