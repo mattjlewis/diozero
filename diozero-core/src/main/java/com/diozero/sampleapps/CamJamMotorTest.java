91c1f888c720235caeeb6fe9ceed37abe8e0d11f
mattjlewis <matthew.lewis@btinternet.com>
2016-03-12 20:58:43 +0000

    Added line following logic. Not yet tested!

--- diozero-core/src/main/java/com/diozero/sampleapps/CamJamMotorTest.java
+++ diozero-core/src/main/java/com/diozero/sampleapps/CamJamMotorTest.java
@@ -32,48 +32,46 @@ import java.io.IOException;
 import org.pmw.tinylog.Logger;
 
 import com.diozero.CamJamKitDualMotor;
+import com.diozero.HCSR04;
 import com.diozero.LED;
 import com.diozero.api.DigitalInputDevice;
+import com.diozero.api.motor.DualMotor;
 import com.diozero.util.SleepUtil;;
 
 public class CamJamMotorTest implements Closeable {
-	private static final int PIN_REAR_LEFT_LED = 22;
-	private static final int PIN_REAR_RIGHT_LED = 27;
-	private static final int PIN_FRONT_LEFT_LED = 18;
-	private static final int PIN_FRONT_RIGHT_LED = 17;
+	private static final int LED_FRONT_LEFT_PIN = 18;
+	private static final int LED_FRONT_RIGHT_PIN = 17;
+	private static final int LED_REAR_LEFT_PIN = 22;
+	private static final int LED_REAR_RIGHT_PIN = 27;
 
-	private static final int PIN_LEFT_IR_SENSOR = 24;
-	private static final int PIN_CENTRE_IR_SENSOR = 23;
-	private static final int PIN_RIGHT_IR_SENSOR = 25;
-
-	private static final int GO_FORWARDS = 1;
-	private static final int GO_BACKWARDS = 2;
-	private static final int GO_LEFT = 3;
-	private static final int GO_RIGHT = 4;
-	private static final int GO_LEFT_BACKWARDS = 5;
-	private static final int GO_RIGHT_BACKWARDS = 6;
-	private static final int GO_SHARP_LEFT = 7;
-	private static final int GO_SHARP_RIGHT = 8;
+	private static final int IR_SENSOR_LEFT_PIN = 24;
+	private static final int IR_SENSOR_CENTRE_PIN = 23;
+	private static final int IR_SENSOR_RIGHT_PIN = 25;
+	
+	private static final int HCSR04_TRIGGER_PIN = 26;
+	private static final int HCSR04_ECHO_PIN = 4;
 
 	public static void main(String[] args) {
 		float DELAY = 0.65f;
 		float speed = 0.9f;
 		
 		try (CamJamMotorTest robot = new CamJamMotorTest()) {
-			robot.go(GO_RIGHT, DELAY, speed);
-			robot.go(GO_FORWARDS, DELAY, speed);
-			robot.go(GO_LEFT, DELAY, speed);
+			robot.go(Direction.RIGHT, DELAY, speed);
+			robot.go(Direction.FORWARDS, DELAY, speed);
+			robot.go(Direction.LEFT, DELAY, speed);
 			robot.stop(DELAY);
-			robot.go(GO_LEFT_BACKWARDS, DELAY, speed);
-			robot.go(GO_BACKWARDS, DELAY, speed);
-			robot.go(GO_RIGHT_BACKWARDS, DELAY, speed);
+			robot.go(Direction.LEFT_BACKWARDS, DELAY, speed);
+			robot.go(Direction.BACKWARDS, DELAY, speed);
+			robot.go(Direction.RIGHT_BACKWARDS, DELAY, speed);
 			robot.stop(DELAY);
-			robot.go(GO_SHARP_LEFT, DELAY, speed);
-			robot.go(GO_SHARP_RIGHT, DELAY, speed);
+			robot.go(Direction.SHARP_LEFT, DELAY, speed);
+			robot.go(Direction.SHARP_RIGHT, DELAY, speed);
 			robot.stop(DELAY);
-			robot.go(GO_FORWARDS, DELAY, speed);
-			robot.go(GO_BACKWARDS, DELAY, speed);
+			robot.go(Direction.FORWARDS, DELAY, speed);
+			robot.go(Direction.BACKWARDS, DELAY, speed);
 			robot.stop(DELAY);
+			
+			robot.followLine();
 		} catch (IOException e) {
 			Logger.error(e, "Error: {}", e);
 		}
@@ -86,24 +84,20 @@ public class CamJamMotorTest implements Closeable {
 	private DigitalInputDevice leftIrSensor;
 	private DigitalInputDevice centreIrSensor;
 	private DigitalInputDevice rightIrSensor;
-	private CamJamKitDualMotor dualMotor;
+	private HCSR04 hcsr04;
+	private DualMotor dualMotor;
 
 	public CamJamMotorTest() {
-		frontLeftLed = new LED(PIN_FRONT_LEFT_LED, true);
-		frontRightLed = new LED(PIN_FRONT_RIGHT_LED, true);
-		rearLeftLed = new LED(PIN_REAR_LEFT_LED, true);
-		rearRightLed = new LED(PIN_REAR_RIGHT_LED, true);
+		frontLeftLed = new LED(LED_FRONT_LEFT_PIN, true);
+		frontRightLed = new LED(LED_FRONT_RIGHT_PIN, true);
+		rearLeftLed = new LED(LED_REAR_LEFT_PIN, true);
+		rearRightLed = new LED(LED_REAR_RIGHT_PIN, true);
 	
-		leftIrSensor = new DigitalInputDevice(PIN_LEFT_IR_SENSOR);
-		centreIrSensor = new DigitalInputDevice(PIN_CENTRE_IR_SENSOR);
-		rightIrSensor = new DigitalInputDevice(PIN_RIGHT_IR_SENSOR);
+		leftIrSensor = new DigitalInputDevice(IR_SENSOR_LEFT_PIN);
+		centreIrSensor = new DigitalInputDevice(IR_SENSOR_CENTRE_PIN);
+		rightIrSensor = new DigitalInputDevice(IR_SENSOR_RIGHT_PIN);
 		
-		leftIrSensor.whenActivated(() -> System.out.println("Left activated"));
-		leftIrSensor.whenDeactivated(() -> System.out.println("Left deactivated"));
-		centreIrSensor.whenActivated(() -> System.out.println("Centre activated"));
-		centreIrSensor.whenDeactivated(() -> System.out.println("Centre deactivated"));
-		rightIrSensor.whenActivated(() -> System.out.println("Right activated"));
-		rightIrSensor.whenDeactivated(() -> System.out.println("Right deactivated"));
+		hcsr04 = new HCSR04(HCSR04_TRIGGER_PIN, HCSR04_ECHO_PIN);
 			
 		dualMotor = new CamJamKitDualMotor();
 		
@@ -111,53 +105,116 @@ public class CamJamMotorTest implements Closeable {
 		frontRightLed.blink(0.25f, 0.25f, 1, false);
 		rearRightLed.blink(0.25f, 0.25f, 1, false);
 		rearLeftLed.blink(0.25f, 0.25f, 1, false);
-		SleepUtil.sleepSeconds(1);
+		
+		Logger.info("Ready");
+	}
+	
+	public void followLine() {
+		float speed = 0.8f;
+		float turn_rate = 0.4f;
+		int delay_ms = 20;
+		
+		while (true) {
+			SleepUtil.sleepMillis(delay_ms);
+			
+			float distance;
+			int count = 0;
+			do {
+				distance = hcsr04.getDistanceCm();
+			} while (distance == -1 && ++count < 10);
+			
+			if (distance != -1) {
+				if (distance < 40) {
+					Logger.info("Object detected");
+				} else if (distance < 5) {
+					Logger.info("Object detected - stopping");
+					dualMotor.stop();
+					continue;
+				}
+			}
+			
+			// Is the centre IR sensor detecting the line?
+			if (centreIrSensor.isActive()) {
+				if (leftIrSensor.isActive() && !rightIrSensor.isActive()) {
+					Logger.info("Drifting left");
+					dualMotor.circleLeft(speed, 0.2f);
+				} else if (rightIrSensor.isActive() && !leftIrSensor.isActive()) {
+					Logger.info("Drifting right");
+					dualMotor.circleRight(speed, 0.2f);
+				} else {
+					Logger.info("Straight-on");
+					dualMotor.forward(speed);
+				}
+			} else {
+				Logger.info("Centre IR sensor has lost the line");
+				if (leftIrSensor.isActive()) {
+					Logger.info("Detected line to the left, turning left");
+					dualMotor.circleLeft(speed, turn_rate);
+				} else if (rightIrSensor.isActive()) {
+					Logger.info("Detected line to the right, turning right");
+					dualMotor.circleRight(speed, turn_rate);
+				} else {
+					Logger.info("Lost the line...");
+					// TODO Is there anything that can be done? Retrace steps to find the line again?
+					dualMotor.stop();
+					break;
+				}
+			}
+		}
 	}
 
-	public void go(int direction, float duration, float speed) {
-		if (direction == GO_FORWARDS) {
+	public void go(Direction direction, float duration, float speed) {
+		switch (direction) {
+		case FORWARDS:
 			frontLeftLed.on();
 			frontRightLed.on();
 			rearLeftLed.off();
 			rearRightLed.off();
 			dualMotor.forward(speed);
-		} else if (direction == GO_BACKWARDS) {
+			break;
+		case BACKWARDS:
 			frontLeftLed.off();
 	    	frontRightLed.off();
 	    	rearLeftLed.on();
 	    	rearRightLed.on();
 	    	dualMotor.backward(speed);
-		} else if (direction == GO_SHARP_LEFT) {
+	    	break;
+		case SHARP_LEFT:
 			frontLeftLed.off();
 			frontRightLed.on();
 			rearLeftLed.off();
 			rearRightLed.on();
 			dualMotor.rotateLeft(speed);
-		} else if (direction == GO_SHARP_RIGHT) {
+			break;
+		case SHARP_RIGHT:
 			frontLeftLed.on();
 			frontRightLed.off();
 			rearLeftLed.on();
 			rearRightLed.off();
 			dualMotor.rotateRight(speed);
-		} else if (direction == GO_LEFT) {
+			break;
+		case LEFT:
 			frontLeftLed.off();
 			frontRightLed.on();
 			rearLeftLed.off();
 			rearRightLed.off();
 			dualMotor.forwardLeft(speed);
-		} else if (direction == GO_RIGHT) {
+			break;
+		case RIGHT:
 			frontLeftLed.on();
 			frontRightLed.off();
 			rearLeftLed.off();
 			rearRightLed.off();
 			dualMotor.forwardRight(speed);
-		} else if (direction == GO_LEFT_BACKWARDS) {
+			break;
+		case LEFT_BACKWARDS:
 			frontLeftLed.off();
 			frontRightLed.off();
 			rearLeftLed.off();
 			rearRightLed.on();
 			dualMotor.backwardLeft(speed);
-		} else if (direction == GO_RIGHT_BACKWARDS) {
+			break;
+		case RIGHT_BACKWARDS:
 			frontLeftLed.off();
 			frontRightLed.off();
 			rearLeftLed.on();
@@ -191,5 +248,9 @@ public class CamJamMotorTest implements Closeable {
 		centreIrSensor.close();
 		rightIrSensor.close();
 		dualMotor.close();
+	}
+	
+	public static enum Direction {
+		FORWARDS, BACKWARDS, LEFT, RIGHT, LEFT_BACKWARDS, RIGHT_BACKWARDS, SHARP_LEFT, SHARP_RIGHT;
 	}
 }
