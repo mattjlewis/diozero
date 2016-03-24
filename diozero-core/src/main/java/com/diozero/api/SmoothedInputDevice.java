c7c27cbeea8f60c10fe6bceffe7ad3cd6692d6ef
mattjlewis <matthew.lewis@btinternet.com>
2016-03-24 09:52:22 +0000

    Added utility operations. Bug fix for active low. Separated event detection period and event age values.

--- diozero-core/src/main/java/com/diozero/api/SmoothedInputDevice.java
+++ diozero-core/src/main/java/com/diozero/api/SmoothedInputDevice.java
@@ -31,54 +31,98 @@ import java.util.Queue;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 
+import com.diozero.internal.DeviceFactoryHelper;
+import com.diozero.internal.spi.GpioDeviceFactoryInterface;
 import com.diozero.util.DioZeroScheduler;
 import com.diozero.util.RuntimeIOException;
 
 /**
- * Represents a generic input device which takes its value from the length of a
- * queue of historical values over a specific time period.
+ * Represents a generic input device which takes its value from the number of active
+ * events over a specific time period.
  * 
- * This class extends <{@link DigitalInputDevice} with a queue which is filled by a
- * background thread which continually polls the state of the underlying
- * device. The mean of the values in the queue is compared to a threshold
- * which is used to determine the state of the 'is_active' property.
+ * This class extends {@link DigitalInputDevice} with a queue which is added to whenever
+ * the input device is active. The number of the active events in the queue is compared
+ * to a threshold which is used to determine the state of the 'active' property.
  * 
- * This class is intended for use with devices which either exhibit analog
- * behaviour (such as the charging time of a capacitor with an LDR), or those
- * which exhibit "twitchy" behaviour (such as certain motion sensors).
+ * Any active events over the specified eventAge are removed by a background thread.
+ * 
+ * This class is intended for use with devices which exhibit "twitchy" behaviour (such
+ * as certain motion sensors).
  */
 public class SmoothedInputDevice extends DigitalInputDevice {
 	private int threshold;
-	private int age;
+	private int eventAge;
+	private int eventDetectPeriod;
 	private Queue<Long> queue;
 
 	/**
 	 * @param pinNumber GPIO pin number
-	 * @param pud pull-up/down configuration
+	 * @param pud Pull-up/down configuration
 	 * @param threshold
 	 * 				The value above which the device will be considered "on".
-	 * @param age
-	 * 				The time in milliseconds to keep items in the queue.
+	 * @param eventAge
+	 * 				The time in milliseconds to keep active events in the queue.
+	 * @param eventDetectPeriod
+	 * 				How frequently to check for events.
 	 * @throws RuntimeIOException if an I/O error occurs
 	 */
 	public SmoothedInputDevice(int pinNumber, GpioPullUpDown pud, int threshold,
-			int age) throws RuntimeIOException {
-		this(pinNumber, pud, threshold, age, GpioEventTrigger.RISING);
+			int eventAge, int eventDetectPeriod) throws RuntimeIOException {
+		this(DeviceFactoryHelper.getNativeDeviceFactory(), pinNumber, pud, threshold, eventAge, eventDetectPeriod);
 	}
 	
-	public SmoothedInputDevice(int pinNumber, GpioPullUpDown pud, int threshold,
-			int age, GpioEventTrigger trigger) throws RuntimeIOException {
-		super(pinNumber, pud, trigger);
+	public SmoothedInputDevice(GpioDeviceFactoryInterface deviceFactory, int pinNumber, GpioPullUpDown pud, int threshold,
+			int eventAge, int eventDetectPeriod) throws RuntimeIOException {
+		super(deviceFactory, pinNumber, pud, pud == GpioPullUpDown.PULL_UP ? GpioEventTrigger.FALLING : GpioEventTrigger.RISING);
 		
 		this.threshold = threshold;
-		this.age = age;
+		this.eventAge = eventAge;
+		this.eventDetectPeriod = eventDetectPeriod;
 		
 		queue = new LinkedList<>();
-		DioZeroScheduler.getDaemonInstance().scheduleAtFixedRate(new EventDetection(), age, age, TimeUnit.MILLISECONDS);
+		DioZeroScheduler.getDaemonInstance().scheduleAtFixedRate(new EventDetection(),
+				eventDetectPeriod, eventDetectPeriod, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	public void valueChanged(DigitalInputEvent event) {
+		event.setActiveHigh(activeHigh);
+		if (event.isActive()) {
+			synchronized (queue) {
+				queue.add(Long.valueOf(event.getEpochTime()));
+			}
+		}
+	}
+	
+	private class EventDetection implements Runnable {
+		private Predicate<Long> removePredicate;
+		private long now;
+		
+		EventDetection() {
+			removePredicate = time -> time.longValue() < (now - eventAge);
+		}
+		
+		@Override
+		public void run() {
+			long nano_time = System.nanoTime();
+			now = System.currentTimeMillis();
+			synchronized (queue) {
+				// Purge any old events
+				queue.removeIf(removePredicate);
+				
+				// Check if the number of events exceeds the threshold
+				if (queue.size() > threshold) {
+					SmoothedInputDevice.super.valueChanged(new DigitalInputEvent(pinNumber, now, nano_time, true));
+					
+					// If an event is fired clear the queue of all events
+					queue.clear();
+				}
+			}
+		}
 	}
 	
 	/**
-	 * If the number of on events younger than age exceeds this amount, then 'isActive' will return 'True'.
+	 * If the number of on events younger than eventAge exceeds this amount, then 'isActive' will return 'True'.
 	 * @return event threshold
 	 */
 	public int getThreshold() {
@@ -88,38 +132,16 @@ public class SmoothedInputDevice extends DigitalInputDevice {
 	public void setThreshold(int threshold) {
 		this.threshold = threshold;
 	}
-
-	@Override
-	public void valueChanged(DigitalInputEvent event) {
-		if (event.isActive()) {
-			synchronized (queue) {
-				queue.add(Long.valueOf(event.getEpochTime()));
-			}
-		}
+	
+	public int getEventAge() {
+		return eventAge;
 	}
 	
-	private class EventDetection implements Runnable {
-		private Predicate<Long> predicate;
-		private long now;
-		
-		EventDetection() {
-			predicate = time -> time.longValue() < (now - age);
-		}
-		
-		@Override
-		public void run() {
-			long nano_time = System.nanoTime();
-			now = System.currentTimeMillis();
-			// Purge any old events
-			synchronized (queue) {
-				queue.removeIf(predicate);
-				// Check if the number of events exceeds the threshold
-				if (queue.size() > threshold) {
-					SmoothedInputDevice.super.valueChanged(new DigitalInputEvent(pinNumber, now, nano_time, true));
-					// If an event is fired clear the queue of all events
-					queue.clear();
-				}
-			}
-		}
+	public void setEventAge(int eventAge) {
+		this.eventAge = eventAge;
+	}
+	
+	public int getEventDetectPeriod() {
+		return eventDetectPeriod;
 	}
 }
