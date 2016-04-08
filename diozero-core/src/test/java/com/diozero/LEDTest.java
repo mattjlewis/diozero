ca9a985636df2368bd4ab8383659612ed8d00261
mattjlewis <matthew.lewis@btinternet.com>
2016-04-08 11:29:34 +0100

    Restored package of DeviceFactoryHelper to internal

--- diozero-core/src/test/java/com/diozero/LEDTest.java
+++ diozero-core/src/test/java/com/diozero/LEDTest.java
@@ -31,11 +31,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.pmw.tinylog.Logger;
 
+import com.diozero.internal.DeviceFactoryHelper;
 import com.diozero.internal.provider.test.TestDeviceFactory;
 import com.diozero.internal.provider.test.TestDigitalInputDevice;
 import com.diozero.internal.provider.test.TestDigitalOutputDevice;
 import com.diozero.internal.spi.NativeDeviceFactoryInterface;
-import com.diozero.util.DeviceFactoryHelper;
 import com.diozero.util.RuntimeIOException;
 
 /**
