635b2eb37ae22268dd9651a52552b00f857f4310
mattjlewis <matthew.lewis@btinternet.com>
2016-04-18 12:21:48 +0100

    Swapped params around - columns then rows

--- diozero-core/src/main/java/com/diozero/sandpit/I2CLcdSampleApp.java
+++ diozero-core/src/main/java/com/diozero/sandpit/I2CLcdSampleApp.java
@@ -6,11 +6,26 @@ import org.pmw.tinylog.Logger;
 
 import com.diozero.util.SleepUtil;
 
+/**
+ * I2C LCD sample application. To run:
+ * <ul>
+ * <li>JDK Device I/O 1.0:<br>
+ *  {@code sudo java -cp tinylog-1.0.3.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-jdkdio10-$DIOZERO_VERSION.jar:dio-1.0.1-dev-linux-armv6hf.jar -Djava.library.path=. com.diozero.sandpit.I2CLcdSampleApp}</li>
+ * <li>JDK Device I/O 1.1:<br>
+ *  {@code sudo java -cp tinylog-1.0.3.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-jdkdio11-$DIOZERO_VERSION.jar:dio-1.1-dev-linux-armv6hf.jar -Djava.library.path=. com.diozero.sandpit.I2CLcdSampleApp}</li>
+ * <li>Pi4j:<br>
+ *  {@code sudo java -cp tinylog-1.0.3.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-pi4j-$DIOZERO_VERSION.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sandpit.I2CLcdSampleApp}</li>
+ * <li>wiringPi:<br>
+ *  {@code sudo java -cp tinylog-1.0.3.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-wiringpi-$DIOZERO_VERSION.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sandpit.I2CLcdSampleApp}</li>
+ * <li>pigpgioJ:<br>
+ *  {@code sudo java -cp tinylog-1.0.3.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-pigpio-$DIOZERO_VERSION.jar:pigpioj-java-1.0.0.jar com.diozero.sandpit.I2CLcdSampleApp}</li>
+ * </ul>
+ */
 public class I2CLcdSampleApp {
 	// Main program block
 	public static void main(String[] args) {
 		// Initialise display
-		try (I2CLcd lcd = new I2CLcd(2, 16)) {
+		try (I2CLcd lcd = new I2CLcd(16, 2)) {
 			while (true) {
 				// Send some text
 				lcd.setText(0, "RPiSpy         <");
