00b3d2605f7ddabef6343ecacb835e14c80b85f8
mattjlewis <matthew.lewis@btinternet.com>
2016-05-31 14:21:25 +0100

    Testing Java access to /dev/mem or /dev/gpiomem via JNI DirectByteBuffer

---
+++ mmap-tests/mmaptest-java/GpioMMapTest.java
@@ -0,0 +1,209 @@
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class GpioMMapTest {
+	private static final boolean READ_ONLY = true;
+	private static final int SIZE_OF_INT = 4;
+	
+	static {
+		System.loadLibrary("gpiommaptest");
+	}
+
+	private static final int PI_MAX_GPIO = 53;
+
+	/*
+ 	0 GPFSEL0   GPIO Function Select 0
+ 	1 GPFSEL1   GPIO Function Select 1
+ 	2 GPFSEL2   GPIO Function Select 2
+ 	3 GPFSEL3   GPIO Function Select 3
+ 	4 GPFSEL4   GPIO Function Select 4
+ 	5 GPFSEL5   GPIO Function Select 5
+ 	6 -         Reserved
+ 	7 GPSET0    GPIO Pin Output Set 0
+ 	8 GPSET1    GPIO Pin Output Set 1
+ 	9 -         Reserved
+	10 GPCLR0    GPIO Pin Output Clear 0
+	11 GPCLR1    GPIO Pin Output Clear 1
+	12 -         Reserved
+	13 GPLEV0    GPIO Pin Level 0
+	14 GPLEV1    GPIO Pin Level 1
+	15 -         Reserved
+	16 GPEDS0    GPIO Pin Event Detect Status 0
+	17 GPEDS1    GPIO Pin Event Detect Status 1
+	18 -         Reserved
+	19 GPREN0    GPIO Pin Rising Edge Detect Enable 0
+	20 GPREN1    GPIO Pin Rising Edge Detect Enable 1
+	21 -         Reserved
+	22 GPFEN0    GPIO Pin Falling Edge Detect Enable 0
+	23 GPFEN1    GPIO Pin Falling Edge Detect Enable 1
+	24 -         Reserved
+	25 GPHEN0    GPIO Pin High Detect Enable 0
+	26 GPHEN1    GPIO Pin High Detect Enable 1
+	27 -         Reserved
+	28 GPLEN0    GPIO Pin Low Detect Enable 0
+	29 GPLEN1    GPIO Pin Low Detect Enable 1
+	30 -         Reserved
+	31 GPAREN0   GPIO Pin Async. Rising Edge Detect 0
+	32 GPAREN1   GPIO Pin Async. Rising Edge Detect 1
+	33 -         Reserved
+	34 GPAFEN0   GPIO Pin Async. Falling Edge Detect 0
+	35 GPAFEN1   GPIO Pin Async. Falling Edge Detect 1
+	36 -         Reserved
+	37 GPPUD     GPIO Pin Pull-up/down Enable
+	38 GPPUDCLK0 GPIO Pin Pull-up/down Enable Clock 0
+	39 GPPUDCLK1 GPIO Pin Pull-up/down Enable Clock 1
+	40 -         Reserved
+	41 -         Test
+	*/
+
+	private static final int GPSET0 = 7;
+	private static final int GPSET1 = 8;
+
+	private static final int GPCLR0 = 10;
+	private static final int GPCLR1 = 11;
+
+	private static final int GPLEV0 = 13;
+	private static final int GPLEV1 = 14;
+
+	private static final int PI_OFF = 0;
+	private static final int PI_ON = 1;
+
+	private static final int PI_CLEAR = 0;
+	private static final int PI_SET = 1;
+
+	private static final int PI_LOW = 0;
+	private static final int PI_HIGH = 1;
+
+	private static final int PI_INPUT = 0;
+	private static final int PI_OUTPUT = 1;
+
+	private static final int PI_ALT0 = 4;
+	private static final int PI_ALT1 = 5;
+	private static final int PI_ALT2 = 6;
+	private static final int PI_ALT3 = 7;
+	private static final int PI_ALT4 = 3;
+	private static final int PI_ALT5 = 2;
+
+	private static final int PI_PUD_OFF = 0;
+	private static final int PI_PUD_DOWN = 1;
+	private static final int PI_PUD_UP = 2;
+
+	public static native ByteBuffer initialise();
+	public static native void terminate();
+
+	//static volatile uint32_t * gpioReg = MAP_FAILED;
+	private static ByteBuffer gpioReg = null;
+
+	public static void main(String[] args) {
+		int gpio = 12;
+
+		try {
+			gpioReg = initialise();
+			if (gpioReg == null) {
+				System.out.println("Error in initialise");
+				return;
+			}
+			System.out.println("order=" + gpioReg.order());
+			gpioReg.order(ByteOrder.LITTLE_ENDIAN);
+			System.out.println("order=" + gpioReg.order());
+			System.out.println("capacity=" + gpioReg.capacity());
+
+			int rc = gpioSetMode(gpio, PI_OUTPUT);
+			if (rc < 0) {
+				System.out.println("Error in gpioSetMode()");
+				return;
+			}
+			delay(1000);
+
+			System.out.println("Turning GPIO " + gpio + " on");
+			rc = gpioWrite(gpio, PI_ON);
+			if (rc < 0) {
+				System.out.println("Error in gpioWrite()");
+				return;
+			}
+			delay(1000);
+
+			System.out.println("Turning GPIO " + gpio + " off");
+			rc = gpioWrite(gpio, PI_OFF);
+			if (rc < 0) {
+				System.out.println("Error in gpioWrite()");
+				return;
+			}
+			delay(1000);
+		} finally {
+			gpioReg = null;
+			terminate();
+		}
+	}
+
+	public static int gpioSetMode(int gpio, int mode) {
+		System.out.println("gpioSetMode(" + gpio + ", " + mode + ")");
+
+		if (gpio > PI_MAX_GPIO || gpio < 0) {
+			return -1;
+		}
+
+		if (mode > PI_ALT3 || mode < 0) {
+			return -2;
+		}
+		
+		for (int i=0; i<GPIO_LEN/SIZE_OF_INT; i++) {
+			System.out.format("gpioReg[%d]=0x%x%n", i, gpioReg.getInt(i*SIZE_OF_INT));
+		}
+
+		int reg = gpio / 10;
+		int shift = (gpio%10) * 3;
+
+		int old_reg_val = gpioReg.getInt(reg*4);
+		int old_mode = (old_reg_val >> shift) & 7;
+		System.out.println("reg=" + reg + ", shift=" + shift + ", old_reg_val=" + old_reg_val + ", old_mode=" + old_mode);
+
+		if (mode != old_mode) {
+			//switchFunctionOff(gpio);
+			int new_mode = (old_reg_val & ~(7<<shift)) | (mode<<shift);
+			System.out.println("new_mode=" + new_mode);
+			
+			if (READ_ONLY) {
+				System.out.println("READ_ONLY flag is true, not writing anything");
+				return -1;
+			}
+			
+			gpioReg.putInt(reg, new_mode);
+		}
+
+		return 0;
+	}
+
+	public static int gpioWrite(int gpio, int level) {
+		if (gpio > PI_MAX_GPIO || gpio < 0) {
+			return -1;
+		}
+
+		if (level > PI_ON || level < 0) {
+			return -2;
+		}
+
+		int BIT = 1 << (gpio & 0x1F);
+		int BANK = gpio >> 5;
+
+		if (READ_ONLY) {
+			System.out.println("READ_ONLY flag is true, not writing anything");
+			return -3;
+		}
+
+		if (level == PI_OFF) {
+			gpioReg.putInt(GPCLR0 + BANK, BIT);
+		} else {
+			gpioReg.putInt(GPSET0 + BANK, BIT);
+		}
+
+		return 0;
+	}
+
+	public static void delay(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+		}
+	}
+}
---
+++ mmap-tests/mmaptest-native/GpioMMapTest.c
@@ -0,0 +1,289 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/file.h>
+#include <jni.h>
+
+#include "GpioMMapTest.h"
+
+// ARMv6
+#define pi_peri_phys_armv6 0x20000000;
+// ARMv7
+#define pi_peri_phys_armv7 0x3F000000;
+
+#define SYST_BASE_OFFSET  0x00003000
+#define GPIO_TIMER_OFFSET 0x0000B000
+#define DMA_BASE_OFFSET   0x00007000
+#define GPIO_PADS_OFFSET  0x00100000
+#define CLK_BASE_OFFSET   0x00101000
+#define GPIO_BASE_OFFSET  0x00200000
+#define PCM_BASE_OFFSET   0x00203000
+#define SPI_BASE_OFFSET   0x00204000
+#define PWM_BASE_OFFSET   0x0020C000
+#define AUX_BASE_OFFSET   0x00215000
+#define DMA15_BASE_OFFSET 0x00E05000
+
+#define GPIO_LEN  0xB4
+
+
+#define GPSET0 7
+#define GPSET1 8
+
+#define GPCLR0 10
+#define GPCLR1 11
+
+#define GPLEV0 13
+#define GPLEV1 14
+
+#define GPPUD     37
+#define GPPUDCLK0 38
+#define GPPUDCLK1 39
+
+#define PI_BANK (gpio>>5)
+#define PI_BIT  (1<<(gpio&0x1F))
+
+#define PI_OFF 0
+#define PI_ON 1
+
+#define PI_CLEAR 0
+#define PI_SET 1
+
+#define PI_LOW 0
+#define PI_HIGH 1
+
+#define PI_INPUT 0
+#define PI_OUTPUT 1
+
+#define PI_ALT0   4
+#define PI_ALT1   5
+#define PI_ALT2   6
+#define PI_ALT3   7
+#define PI_ALT4   3
+#define PI_ALT5   2
+
+#define	PAGE_SIZE		(4*1024)
+#define	BLOCK_SIZE		(4*1024)
+
+static volatile uint32_t piPeriBase;
+static volatile uint32_t gpioPads;
+static volatile uint32_t gpioClockBack;
+static volatile uint32_t gpioBase;
+static volatile uint32_t gpioTimer;
+static volatile uint32_t gpioPwm;
+
+static int fdMem = -1;
+static uint32_t *gpioAddr = MAP_FAILED;
+
+// gpioToGPFSEL:
+//	Map a BCM_GPIO pin to it's Function Selection
+//	control port. (GPFSEL 0-5)
+//	Groups of 10 - 3 bits per Function - 30 bits per port
+// = gpio / 10
+
+static uint8_t gpioToGPFSEL [] =
+{
+  0,0,0,0,0,0,0,0,0,0,
+  1,1,1,1,1,1,1,1,1,1,
+  2,2,2,2,2,2,2,2,2,2,
+  3,3,3,3,3,3,3,3,3,3,
+  4,4,4,4,4,4,4,4,4,4,
+  5,5,5,5,5,5,5,5,5,5,
+} ;
+
+
+// gpioToShift
+//	Define the shift up for the 3 bits per pin in each GPFSEL port
+// = (gpio % 10) * 3
+
+static uint8_t gpioToShift [] =
+{
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+} ;
+
+// gpioToGPSET:
+//	(Word) offset to the GPIO Set registers for each GPIO pin
+
+static uint8_t gpioToGPSET [] =
+{
+   7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+   8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+} ;
+
+// gpioToGPCLR:
+//	(Word) offset to the GPIO Clear registers for each GPIO pin
+
+static uint8_t gpioToGPCLR [] =
+{
+  10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,
+  11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,
+} ;
+
+
+// gpioToGPLEV:
+//	(Word) offset to the GPIO Input level registers for each GPIO pin
+
+static uint8_t gpioToGPLEV [] =
+{
+  13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,
+  14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,
+} ;
+
+/* Java VM interface */
+static JavaVM* globalJavaVM = NULL;
+
+JavaVM* getGlobalJavaVM() {
+	return globalJavaVM;
+}
+
+/* The VM calls this function upon loading the native library. */
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* jvm, void* reserved) {
+	printf("JNI_OnLoad()\n");
+	globalJavaVM = jvm;
+
+	return JNI_VERSION_1_8;
+}
+
+/*
+ * Class:     GpioMMapTest
+ * Method:    initialise
+ * Signature: ()Ljava/nio/ByteBuffer;
+ */
+JNIEXPORT jobject JNICALL Java_GpioMMapTest_initialise
+  (JNIEnv *env, jclass clz) {
+
+	int useGpioMem = 0;
+
+	if (useGpioMem) {
+		piPeriBase = 0;
+		fdMem = open("/dev/gpiomem", O_RDWR | O_SYNC | O_CLOEXEC);
+	} else {
+		piPeriBase = pi_peri_phys_armv7;
+		fdMem = open("/dev/mem", O_RDWR | O_SYNC | O_CLOEXEC);
+	}
+	if (fdMem < 0) {
+		printf("You don't have permission to run this program, try running as root\n");
+		return NULL;
+	}
+
+	// Set the offsets into the memory interface.
+	gpioPads 	  = piPeriBase + GPIO_PADS_OFFSET;
+	gpioClockBase = piPeriBase + CLK_BASE_OFFSET;
+	gpioBase	  = piPeriBase + GPIO_BASE_OFFSET;
+	gpioTimer	  = piPeriBase + GPIO_TIMER_OFFSET;
+	gpioPwm		  = piPeriBase + PWM_BASE_OFFSET;
+
+	printf("GPIO_BASE=0x%x, GPIO_LEN=0x%x\n", GPIO_BASE, GPIO_LEN);
+	//gpioAddr = mmap(0, GPIO_LEN, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED|MAP_LOCKED, fdMem, GPIO_BASE);
+	gpioAddr = (uint32_t *)mmap(NULL, BLOCK_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fdMem, gpioBase);
+	printf("gpioAddr=%p\n", (void*)gpioAddr);
+
+	if ((int32_t)gpioAddr == MAP_FAILED) {
+		printf("mmap failed\n");
+		return NULL;
+	}
+
+	if (env == NULL) {
+		return NULL;
+	}
+
+	jobject direct_buffer = (*env)->NewDirectByteBuffer(env, gpioAddr, GPIO_LEN);
+
+	return direct_buffer;
+}
+
+/*
+ * Class:     GpioMMapTest
+ * Method:    terminate
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_GpioMMapTest_terminate
+  (JNIEnv *env, jclass clz) {
+	munmap((void *)gpioAddr, GPIO_LEN);
+	close(fdMem);
+}
+
+int main(int argc, char *argv[]) {
+	Java_GpioMMapTest_initialise(NULL, NULL);
+	if (gpioAddr == MAP_FAILED) {
+		printf("Error in initialise()\n");
+		return -1;
+	}
+
+	printf("gpioAddr=0x%p\n", (void*)gpioAddr);
+	printf("&gpioAddr[0]=0x%p\n", (void*)&gpioAddr[0]);
+	printf("&gpioAddr[1]=0x%p\n", (void*)&gpioAddr[1]);
+	printf("&gpioAddr[2]=0x%p\n", (void*)&gpioAddr[2]);
+
+	printf("gpioAddr[0]=0x%x\n", gpioAddr[0]);
+	printf("gpioAddr[1]=0x%x\n", gpioAddr[1]);
+	printf("gpioAddr[2]=0x%x\n", gpioAddr[2]);
+	printf("gpioAddr[3]=0x%x\n", gpioAddr[3]);
+
+	int gpio_pin = 12;
+
+	int reg = gpio_pin / 10;
+	int shift = (gpio_pin % 10) * 3;
+
+	int old_reg_val = gpioAddr[reg];
+	int old_mode = (gpioAddr[reg] >> shift) & 7;
+	int old_mode2 = (*(gpioAddr + reg) >> shift) & 7;
+	printf("reg=%d, shift=%d, old_reg_val=0x%x, old_mode=0x%x, old_mode2=0x%x\n", reg, shift, old_reg_val, old_mode, old_mode2);
+
+	printf("Setting pin mode...\n");
+	setPinMode(gpio_pin, PI_OUTPUT);
+	sleep(1);
+	printf("Setting high...\n");
+	gpioWrite(gpio_pin, PI_HIGH);
+	sleep(1);
+	printf("Setting low...\n");
+	gpioWrite(gpio_pin, PI_LOW);
+	sleep(1);
+
+	printf("Done.\n");
+	Java_GpioMMapTest_terminate(NULL, NULL);
+
+	return 0;
+}
+
+int getPinMode(unsigned int gpioPin) {
+	int fSel = gpioToGPFSEL[gpioPin];
+	int shift = gpioToShift[gpioPin];
+
+	return (gpioAddr[fSel] >> shift) & 7;
+}
+
+void setPinMode(unsigned int gpioPin, unsigned int mode) {
+	int fSel = gpioToGPFSEL[gpioPin];
+	int shift = gpioToShift[gpioPin];
+
+	if (mode == PI_INPUT) {
+		*(gpioAddr + fSel) = (*(gpioAddr + fSel) & ~(7 << shift)); // Sets bits to zero = input
+	} else if (mode == PI_OUTPUT) {
+		*(gpioAddr + fSel) = (*(gpioAddr + fSel) & ~(7 << shift)) | (1 << shift);
+	}
+}
+
+int gpioRead(unsigned int gpioPin) {
+	if ((*(gpioAddr + gpioToGPLEV[gpioPin]) & (1 << (gpioPin & 31))) != 0) {
+		return PI_HIGH;
+	} else {
+		return PI_LOW;
+	}
+}
+
+void gpioWrite(unsigned int gpioPin, unsigned int level) {
+	if (value == PI_LOW) {
+		*(gpioAddr + gpioToGPCLR[gpioPin]) = 1 << (gpioPin & 31);
+	} else {
+		*(gpioAddr + gpioToGPSET[gpioPin]) = 1 << (gpioPin & 31);
+	}
+}
