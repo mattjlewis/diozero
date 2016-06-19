48dc91420f2166f4a97ff4279c177360d45d259f
mattjlewis <matthew.lewis@btinternet.com>
2016-06-19 19:30:04 +0100

    Java WatchService doesn't work with /sys file-system, use native poll instead

--- diozero-provider-sysfs/src/main/java/com/diozero/internal/provider/sysfs/SysFsGpioInputDevice.java
+++ diozero-provider-sysfs/src/main/java/com/diozero/internal/provider/sysfs/SysFsGpioInputDevice.java
@@ -1,44 +1,70 @@
 package com.diozero.internal.provider.sysfs;
 
+/*
+ * #%L
+ * Device I/O Zero - Java Sysfs provider
+ * %%
+ * Copyright (C) 2016 mattjlewis
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.file.*;
+import java.nio.file.Path;
 
 import org.pmw.tinylog.Logger;
 
 import com.diozero.api.DigitalInputEvent;
 import com.diozero.api.GpioEventTrigger;
-import com.diozero.api.GpioPullUpDown;
 import com.diozero.internal.spi.AbstractInputDevice;
 import com.diozero.internal.spi.GpioDigitalInputDeviceInterface;
-import com.diozero.util.DioZeroScheduler;
-import com.diozero.util.RuntimeIOException;
+import com.diozero.util.*;
 
 /**
  * Pure Java implementation using <a href="https://www.kernel.org/doc/Documentation/gpio/sysfs.txt">/sys/class/gpio</a> kernel module
  *
  */
 public class SysFsGpioInputDevice extends AbstractInputDevice<DigitalInputEvent>
-implements GpioDigitalInputDeviceInterface, Runnable {
+implements GpioDigitalInputDeviceInterface, PollEventListener, Runnable {
 	private static final String EDGE_FILE = "edge";
 	private static final String VALUE_FILE = "value";
 	//private static final byte LOW_VALUE = '0';
 	private static final byte HIGH_VALUE = '1';
 	
+	private PollNative pollNative;
 	private SysFsDeviceFactory deviceFactory;
 	private int pinNumber;
+	private Path valuePath;
 	private RandomAccessFile valueFile;
-	private boolean watchServiceRunning;
-	private WatchService valueWatchService;
-	private Thread watchServiceThread;
 
 	public SysFsGpioInputDevice(SysFsDeviceFactory deviceFactory, Path gpioDir,
-			String key, int pinNumber, GpioPullUpDown pud, GpioEventTrigger trigger) {
+			String key, int pinNumber, GpioEventTrigger trigger) {
 		super(key, deviceFactory);
 		
 		this.deviceFactory = deviceFactory;
 		this.pinNumber = pinNumber;
+		
+		pollNative = new PollNative();
 
 		try (FileWriter writer = new FileWriter(gpioDir.resolve(EDGE_FILE).toFile())) {
 			writer.write(trigger.name().toLowerCase());
@@ -49,56 +75,12 @@ implements GpioDigitalInputDeviceInterface, Runnable {
 		// Note: Not possible to set pull-up/down resistor configuration via /sys/class/gpio
 		// TODO Set active_low based on the pud value?
 		
-		Path value_path = gpioDir.resolve(VALUE_FILE);
+		valuePath = gpioDir.resolve(VALUE_FILE);
 		try {
-			valueFile = new RandomAccessFile(value_path.toFile(), "r");
+			valueFile = new RandomAccessFile(valuePath.toFile(), "r");
 		} catch (IOException e) {
 			throw new RuntimeIOException("Error opening value file for GPIO " + pinNumber, e);
 		}
-
-		// FIXME WatchService doesn't work with /sys, /proc and network file-systems
-		// http://stackoverflow.com/questions/30190730/nio-watchservice-for-unix-sys-classes-gpio-files
-		try {
-			valueWatchService = FileSystems.getDefault().newWatchService();
-			gpioDir.register(valueWatchService, StandardWatchEventKinds.ENTRY_MODIFY);
-		} catch (IOException e) {
-			throw new RuntimeIOException(e);
-		}
-	}
-	
-	@Override
-	public void run() {
-		watchServiceThread = Thread.currentThread();
-		watchServiceRunning = true;
-		try {
-			while (watchServiceRunning) {
-				// Wait for a change
-				WatchKey key = valueWatchService.take();
-				Logger.debug("Got WatchKey '" + key + "'");
-				long epoch_time = System.currentTimeMillis();
-				long nano_time = System.nanoTime();
-				for (WatchEvent<?> event: key.pollEvents()) {
-					WatchEvent.Kind<?> kind = event.kind();
-					Logger.debug("Got WatchEvent '" + event + "'");
-					if (kind != StandardWatchEventKinds.ENTRY_MODIFY) {
-						continue;
-					}
-					
-					Path path = (Path) event.context();
-					Logger.debug("Got WatchEvent Path '" + path + "'");
-					if (path.endsWith(VALUE_FILE)) {
-						valueChanged(new DigitalInputEvent(pinNumber, epoch_time, nano_time, getValue()));
-					}
-				}
-				if (! key.reset()) {
-					Logger.error("Error, key.reset() return false");
-					break;
-				}
-			}
-		} catch (InterruptedException e) {
-			// Ignore
-		}
-		watchServiceRunning = false;
 	}
 
 	@Override
@@ -123,16 +105,17 @@ implements GpioDigitalInputDeviceInterface, Runnable {
 
 	@Override
 	public void enableListener() {
+		Logger.info("enableListener()");
 		disableListener();
-		DioZeroScheduler.getNonDaemonInstance().execute(this);
+		// Note WatchService doesn't work with /sys, /proc and network file-systems
+		// http://stackoverflow.com/questions/30190730/nio-watchservice-for-unix-sys-classes-gpio-files
+		Logger.info("Starting thread to poll '" + valuePath + "'");
+		DioZeroScheduler.getDaemonInstance().execute(this);
 	}
 
 	@Override
 	public void disableListener() {
-		watchServiceRunning = false;
-		if (watchServiceThread != null) {
-			watchServiceThread.interrupt();
-		}
+		pollNative.stop();
 	}
 
 	@Override
@@ -140,11 +123,22 @@ implements GpioDigitalInputDeviceInterface, Runnable {
 		Logger.debug("closeDevice()");
 		disableListener();
 		try {
-			valueWatchService.close();
 			valueFile.close();
 		} catch (IOException e) {
 			throw new RuntimeIOException(e);
 		}
 		deviceFactory.unexport(pinNumber);
+	}
+
+	@Override
+	public void notify(int ref, long epochTime) {
+		Logger.info("notify(" + ref + ", " + epochTime + ")");
+		valueChanged(new DigitalInputEvent(pinNumber, epochTime, System.nanoTime(), getValue()));
+	}
+
+	@Override
+	public void run() {
+		pollNative.poll(valuePath.toString(), 100, pinNumber, this);
+		Logger.info("poll returned");
 	}
 }
