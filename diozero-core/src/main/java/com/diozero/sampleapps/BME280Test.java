fb26241df6d30106936767734f1efc68d75e60aa
mattjlewis <matthew.lewis@btinternet.com>
2016-06-24 09:47:11 +0100

    Added

---
+++ diozero-core/src/main/java/com/diozero/sampleapps/BME280Test.java
@@ -0,0 +1,19 @@
+package com.diozero.sampleapps;
+
+import com.diozero.BME280;
+import com.diozero.util.SleepUtil;
+
+public class BME280Test {
+	public static void main(String[] args) {
+		try (BME280 bme280 = new BME280()) {
+			for (int i=0; i<10; i++) {
+				double[] tph = bme280.getValues();
+				System.out.printf("Temperature in Celsius : %.2f C %n", Double.valueOf(tph[0]));
+				System.out.printf("Pressure : %.2f hPa %n", Double.valueOf(tph[1]));
+				System.out.printf("Relative Humidity : %.2f %% RH %n", Double.valueOf(tph[2]));
+				
+				SleepUtil.sleepSeconds(1);
+			}
+		}
+	}
+}
