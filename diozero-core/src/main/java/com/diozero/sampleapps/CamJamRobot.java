7efe4da759ab5a32ef4ae4f22f9462586e4216be
mattjlewis <matthew.lewis@btinternet.com>
2016-12-02 12:35:18 +0000

    Added

---
+++ diozero-core/src/main/java/com/diozero/sampleapps/CamJamRobot.java
@@ -0,0 +1,64 @@
+package com.diozero.sampleapps;
+
+/*
+ * #%L
+ * Device I/O Zero - Core
+ * %%
+ * Copyright (C) 2016 mattjlewis
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+
+import org.pmw.tinylog.Logger;
+
+import com.diozero.CamJamKitDualMotor;
+import com.diozero.util.RuntimeIOException;;
+
+/**
+ * Robot test application with CamJam EduKit motor controller, 3 IR sensors, an HC-SR04 distance sensor and 4 LEDs. To run:
+ * <ul>
+ * <li>Pi4j:<br>
+ *  {@code sudo java -cp tinylog-1.1.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-pi4j-$DIOZERO_VERSION.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sampleapps.CamJamRobot}</li>
+ * <li>wiringPi:<br>
+ *  {@code sudo java -cp tinylog-1.1.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-wiringpi-$DIOZERO_VERSION.jar:pi4j-core-1.1-SNAPSHOT.jar com.diozero.sampleapps.CamJamRobot}</li>
+ * <li>pigpgioJ:<br>
+ *  {@code sudo java -cp tinylog-1.1.jar:diozero-core-$DIOZERO_VERSION.jar:diozero-provider-pigpio-$DIOZERO_VERSION.jar:pigpioj-java-1.0.0.jar com.diozero.sampleapps.CamJamRobot}</li>
+ * </ul>
+ */
+public class CamJamRobot extends RobotTest {
+	public static void main(String[] args) {
+		float speed = 0.9f;
+		float DELAY = 0.65f;
+		
+		try (CamJamRobot robot = new CamJamRobot()) {
+			robot.testMovements(speed, DELAY);
+			
+			robot.go();
+		} catch (RuntimeIOException e) {
+			Logger.error(e, "Error: {}", e);
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	public CamJamRobot() {
+		super(new CamJamKitDualMotor());
+	}
+}
