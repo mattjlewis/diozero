407965fe0f14e5ecc6c1bd535ec408bae296249d
mattjlewis <matthew.lewis@btinternet.com>
2016-12-19 19:20:29 +0000

    Added CE constants

--- diozero-core/src/main/java/com/diozero/api/SPIConstants.java
+++ diozero-core/src/main/java/com/diozero/api/SPIConstants.java
@@ -32,4 +32,8 @@ public interface SPIConstants {
 	public static final int DEFAULT_SPI_CONTROLLER = 0;
 	public static final int DEFAULT_SPI_CLOCK_FREQUENCY = 500_000;
 	public static final SpiClockMode DEFAULT_SPI_CLOCK_MODE = SpiClockMode.MODE_1;
+	public static final int CE0 = 0;
+	public static final int CE1 = 1;
+	public static final int CE2 = 2;
+	public static final int CE3 = 3;
 }
