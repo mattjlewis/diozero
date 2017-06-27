793fb0fed7dfa6966663f93fc0ea3b06f5fbaac2
mattjlewis <matthew.lewis@btinternet.com>
2017-06-27 14:49:18 +0100

    Update licence header

--- diozero-sampleapps/src/main/java/com/diozero/sampleapps/PiconZeroTest.java
+++ diozero-sampleapps/src/main/java/com/diozero/sampleapps/PiconZeroTest.java
@@ -2,7 +2,12 @@ package com.diozero.sampleapps;
 
 /*-
  * #%L
- * Device I/O Zero - Sample applications
+ * Organisation: mattjlewis
+ * Project:      Device I/O Zero - Sample applications
+ * Filename:     PiconZeroTest.java  
+ * 
+ * This file is part of the diozero project. More information about this project
+ * can be found at http://www.diozero.com/
  * %%
  * Copyright (C) 2016 - 2017 mattjlewis
  * %%
@@ -25,6 +30,7 @@ package com.diozero.sampleapps;
  * THE SOFTWARE.
  * #L%
  */
+
 
 import com.diozero.devices.PiconZero;
 import com.diozero.devices.PiconZero.OutputConfig;
