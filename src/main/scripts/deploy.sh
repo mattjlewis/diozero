c42ea52ba96f5cfdd0f00b034b9b811502535c6c
mattjlewis <matthew.lewis@btinternet.com>
2017-08-08 17:25:20 +0100

    Added remote server and provider projects to Maven

--- src/main/scripts/deploy.sh
+++ src/main/scripts/deploy.sh
@@ -54,11 +54,13 @@ files="../pigpioj/pigpioj-java/target/pigpioj-java-${pigpioj_version}.jar \
 	diozero-imu-devices/target/diozero-imu-devices-${version}.jar \
 	diozero-imu-sampleapp/target/diozero-imu-sampleapp-${version}.jar \
 	diozero-imu-visualiser/target/diozero-imu-visualiser-${version}.jar \
+	diozero-remote-server/target/diozero-remote-server-${version}.jar \
 	diozero-provider-bbbiolib/target/diozero-provider-bbbiolib-${version}.jar \
 	diozero-provider-firmata/target/diozero-provider-firmata-${version}.jar \
 	diozero-provider-jdkdio10/target/diozero-provider-jdkdio10-${version}.jar \
 	diozero-provider-jdkdio11/target/diozero-provider-jdkdio11-${version}.jar \
 	diozero-provider-mmap/target/diozero-provider-mmap-${version}.jar \
+	diozero-provider-remote/target/diozero-provider-remote-${version}.jar \
 	diozero-provider-pi4j/target/diozero-provider-pi4j-${version}.jar \
 	diozero-provider-pigpio/target/diozero-provider-pigpio-${version}.jar \
 	diozero-provider-voodoospark/target/diozero-provider-voodoospark-${version}.jar \
@@ -66,7 +68,6 @@ files="../pigpioj/pigpioj-java/target/pigpioj-java-${pigpioj_version}.jar \
 	diozero-ws281x-java/target/diozero-ws281x-java-${version}.jar \
 	diozero-sampleapps/target/diozero-sampleapps-${version}.jar \
 	diozero-webapp/target/diozero-webapp-${version}.jar \
-	diozero-mqtt-server/target/diozero-mqtt-server-${version}.jar \
 	distribution/target/diozero-distribution-${version}-bin.zip"
 
 scp $files ${username}@${host}:${install_folder}
