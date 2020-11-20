584240ac058c3411453dd22637d1a6c4f27fe797
Matthew Lewis <deviceiozero@gmail.com>
2020-11-20 13:55:08 +0000

    Quick hack fix to OdroidC2MmapGpio for ARmbian 5.x kernels

--- diozero-core/src/main/java/com/diozero/internal/board/odroid/OdroidC2MmapGpio.java
+++ diozero-core/src/main/java/com/diozero/internal/board/odroid/OdroidC2MmapGpio.java
@@ -57,22 +57,25 @@ public class OdroidC2MmapGpio implements MmapGpioInterface {
 	private static final int GPIO_BASE_OFFSET = 0xC8834000;
 	private static final int BLOCK_SIZE = 4*1024;
 	private static final int C2_GPIO_PIN_BASE = 0x88;
-	private static final int C2_GPIOY_PIN_START = C2_GPIO_PIN_BASE + 0x4b;
-	private static final int C2_GPIOY_PIN_END = C2_GPIO_PIN_BASE + 0x5b;
-	private static final int C2_GPIOX_PIN_START = C2_GPIO_PIN_BASE + 0x5c;
-	private static final int C2_GPIOX_PIN_END = C2_GPIO_PIN_BASE + 0x72;
+	
+	// XXX Armbian Kernel 5.x has these starting at an addition offset of 242 (0xF2)
+	private static final int C2_GPIOY_PIN_START = C2_GPIO_PIN_BASE + 0x4b + 242;
+	private static final int C2_GPIOY_PIN_END = C2_GPIO_PIN_BASE + 0x5b + 242;
 
-	private static final int C2_GPIOX_FSEL_REG_OFFSET = 0x118;
-	private static final int C2_GPIOX_OUTP_REG_OFFSET = 0x119;
-	private static final int C2_GPIOX_INP_REG_OFFSET = 0x11A;
-	private static final int C2_GPIOX_PUPD_REG_OFFSET = 0x13E;
-	private static final int C2_GPIOX_PUEN_REG_OFFSET = 0x14C;
+	private static final int C2_GPIOX_PIN_START = C2_GPIO_PIN_BASE + 0x5c + 242;
+	private static final int C2_GPIOX_PIN_END = C2_GPIO_PIN_BASE + 0x72 + 242;
 
 	private static final int C2_GPIOY_FSEL_REG_OFFSET = 0x10F;
 	private static final int C2_GPIOY_OUTP_REG_OFFSET = 0x110;
 	private static final int C2_GPIOY_INP_REG_OFFSET = 0x111;
 	private static final int C2_GPIOY_PUPD_REG_OFFSET = 0x13B;
 	private static final int C2_GPIOY_PUEN_REG_OFFSET = 0x149;
+	
+	private static final int C2_GPIOX_FSEL_REG_OFFSET = 0x118;
+	private static final int C2_GPIOX_OUTP_REG_OFFSET = 0x119;
+	private static final int C2_GPIOX_INP_REG_OFFSET = 0x11A;
+	private static final int C2_GPIOX_PUPD_REG_OFFSET = 0x13E;
+	private static final int C2_GPIOX_PUEN_REG_OFFSET = 0x14C;
 	
 	private static final int[] C2_GP_TO_SHIFT_REG = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
 			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
