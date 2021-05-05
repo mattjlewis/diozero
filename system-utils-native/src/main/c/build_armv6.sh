0707154bd2d1e92e6e690183ac66d864e4c5598a
Matthew Lewis <deviceiozero@gmail.com>
2021-05-05 18:47:35 +0100

    Tweaking cross-compilation

--- system-utils-native/src/main/c/build_armv6.sh
+++ system-utils-native/src/main/c/build_armv6.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 PATH=/home/vagrant/rpi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin:$PATH
-make clean && make CROSS_PREFIX=arm-linux-gnueabihf- ARCH=armv6 CC_CFLAGS="-mfpu=vfp -mfloat-abi=hard"
+CROSS_PREFIX=arm-linux-gnueabihf-
+make clean && make CROSS_PREFIX=${CROSS_PREFIX} ARCH=armv6 CC_CFLAGS="-mfpu=vfp -mfloat-abi=hard" CC_LFLAGS="-L/home/vagrant/libs/${CROSS_PREFIX}lib"
 OUTPUT_DIR=armv6
 mkdir -p ${OUTPUT_DIR}
 cp libdiozero-system-utils.so ${OUTPUT_DIR}/libdiozero-system-utils.so
