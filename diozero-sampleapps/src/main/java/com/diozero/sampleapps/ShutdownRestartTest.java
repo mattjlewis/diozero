ba1a8ccd94a42801e9e915aeb04ee1b2b9a310b3
Matthew Lewis <deviceiozero@gmail.com>
2021-05-14 11:07:05 +0100

    Additional test withoput shutting down the device factory

--- diozero-sampleapps/src/main/java/com/diozero/sampleapps/ShutdownRestartTest.java
+++ diozero-sampleapps/src/main/java/com/diozero/sampleapps/ShutdownRestartTest.java
@@ -9,10 +9,31 @@ import com.diozero.util.SleepUtil;
 
 public class ShutdownRestartTest {
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
+		if (args.length < 2) {
+			System.out.println("Usage: " + ShutdownRestartTest.class.getName() + " <out-gpio> <in-gpio>");
+			return;
+		}
+		int out_gpio = Integer.parseInt(args[0]);
+		int in_gpio = Integer.parseInt(args[1]);
+
+		for (int i = 0; i < 2; i++) {
 			Logger.info("Loop #{}", Integer.valueOf(i));
-			try (LED led = new LED(21); Button button = new Button(5)) {
-				for (int x = 0; x < 5; x++) {
+			try (LED led = new LED(out_gpio); Button button = new Button(in_gpio)) {
+				for (int x = 0; x < 2; x++) {
+					led.on();
+					SleepUtil.sleepMillis(500);
+					led.off();
+					SleepUtil.sleepMillis(500);
+				}
+			}
+
+			SleepUtil.sleepMillis(500);
+		}
+
+		for (int i = 0; i < 2; i++) {
+			Logger.info("Loop #{}", Integer.valueOf(i));
+			try (LED led = new LED(out_gpio); Button button = new Button(in_gpio)) {
+				for (int x = 0; x < 2; x++) {
 					led.on();
 					SleepUtil.sleepMillis(500);
 					led.off();
