d766c314639d4cff69635c3b9165e5712e78c63c
Mariusz Józala <qmy@kmd.dk>
2022-07-28 14:22:37 +0200

    Do not set all pins on MCP23xxx to zero when initialising It is possible to set them to zero if needed, after initialisation. However, when all pins are set to off during initialisation, it may cause some side effects (like lights blinking) on application restart.

--- diozero-core/src/main/java/com/diozero/devices/mcp23xxx/MCP23xxx.java
+++ diozero-core/src/main/java/com/diozero/devices/mcp23xxx/MCP23xxx.java
@@ -5,7 +5,7 @@ package com.diozero.devices.mcp23xxx;
  * Organisation: diozero
  * Project:      diozero - Core
  * Filename:     MCP23xxx.java
- * 
+ *
  * This file is part of the diozero project. More information about this project
  * can be found at https://www.diozero.com/.
  * %%
@@ -17,10 +17,10 @@ package com.diozero.devices.mcp23xxx;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -203,8 +203,6 @@ public abstract class MCP23xxx extends AbstractDeviceFactory implements GpioDevi
 			writeByte(getIntConReg(port), interruptCompareFlags[port].getValue());
 			// Disable pull-up resistors
 			writeByte(getGPPullUpReg(port), pullUps[port].getValue());
-			// Set all values to off
-			writeByte(getGPIOReg(port), (byte) 0);
 		}
 
 		// Finally enable interrupt listeners
